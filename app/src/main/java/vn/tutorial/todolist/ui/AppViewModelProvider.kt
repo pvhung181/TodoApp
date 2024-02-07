ac1149c32282e0e888bea1399ad8b0e87844af10
pvhung <hungdev27102003@gmail.com>
2024-02-07 23:34:56 +0700

    add ViewModelProvider

---
+++ app/src/main/java/vn/tutorial/todolist/ui/AppViewModelProvider.kt
@@ -0,0 +1,29 @@
+package vn.tutorial.todolist.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import vn.tutorial.todolist.TodoApplication
+import vn.tutorial.todolist.ui.screen.home.DetailCategoryViewModel
+import vn.tutorial.todolist.ui.screen.home.HomeViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                taskRepository = todoApplication().container.taskRepository,
+                categoryRepository = todoApplication().container.categoryRepository
+            )
+        }
+
+        initializer {
+            DetailCategoryViewModel(
+                taskRepository = todoApplication().container.taskRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.todoApplication(): TodoApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TodoApplication)
